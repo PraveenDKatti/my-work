8ed9487444a6bbf2a7fe7ea4ac76d6fe6579b365
Praveen Katti <praveendk25@gmail.com>
2025-12-17 23:32:32 +0530

    added new Project and corrected project card.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,16 @@
+
 const Projects = () => {
   const projects = [
-    { title: "Teerex", img: "https://s3.eu-central-2.wasabisys.com/w.storage.screenshotapi.net/teerexmark_netlify_app_9629ddf1398a.png" },
-    { title: "Project Two", img: "./assets/project-2.png" },
+    { title: "Teerex",
+      img: "./assets/teerexmark.png",
+      github: "https://github.com/PraveenDKatti/geektrust/tree/main/TeeRex-Store",
+      liveDemo: "https://teerexmark.netlify.app/"
+    },
+    { title: "Habitore",
+      img: "./assets/habitore.png",
+      github: "https://github.com/PraveenDKatti/habitore",
+      liveDemo: "https://habitore.vercel.app/",
+    },
     { title: "Project Three", img: "./assets/project-3.png" },
   ];
 
@@ -16,19 +25,19 @@ const Projects = () => {
             <div className="h-[300px] w-full rounded-[2rem] overflow-hidden mb-4">
               <img src={project.img} 
               alt={project.title} 
-              className="w-full h-full object-cover" />
+              className="w-full h-full" />
             </div>
             <h2 className="text-[1.75rem] font-bold my-4">{project.title}</h2>
             <div className="flex justify-center gap-4">
               <button 
                 className="btn font-semibold p-4 w-32 rounded-[2rem] border border-gray-light hover:bg-gray-800 hover:text-white transition-all duration-300"
-                onClick={() => window.open('https://github.com/PraveenDKatti/geektrust/tree/main/TeeRex-Store')}
+                onClick={() => window.open(project.github)}
               >
                 Github
               </button>
               <button 
                 className="btn font-semibold p-4 w-32 rounded-[2rem] border border-gray-light hover:bg-gray-800 hover:text-white transition-all duration-300"
-                onClick={() => window.open('https://teerexmark.netlify.app/')}
+                onClick={() => window.open(project.liveDemo)}
               >
                 Live Demo
               </button>
